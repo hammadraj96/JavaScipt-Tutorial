17894f323981e331230b9ff0668b5fa41a9b31d5
hammadraj96 <hammadraj96@gmail.com>
2024-08-13 04:15:53 +0000

    variabels

---
+++ 01_basics/01_variabels.js
@@ -0,0 +1,14 @@
+// Variabels
+const accountID = 36102;
+let accountEmail = "hammadraj96@gmail.com";
+var accountPassword = "78459612";
+accountCity = "Multan"
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+accountEmail = "hammadraj25@gmail.com";
+accountPassword = "0123975";
+accountCity = "Khanewal";
+
+console.table([accountID, accountEmail, accountPassword, accountCity]);
